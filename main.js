d6db3d9df61dcc849a8027886adb3d4cc2ff404d
Merkury32 <pjozefiak32@gmail.com>
2020-02-27 18:19:33 +0100

    naprawiłem błąd z pozycjonowaniem piksela

--- main.js
+++ main.js
@@ -1,17 +1,29 @@
-function putPixel(x, y, r, g, b, a) {
+function putPixel() {
+  debugger;
   var canvas = document.getElementById('canvas');
   var ctx = canvas.getContext('2d');
+
   var canvasWidth = canvas.width;
   var canvasHeight = canvas.height;
   ctx.clearRect(0, 0, canvasWidth, canvasHeight);
   var id = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
   var pixels = id.data;
+  
+    var tmpa = document.getElementById("x-input").value;
+    var tmpb = document.getElementById("y-input").value;
+
+    var x = parseInt(tmpa);
+    var y = parseInt(tmpb);
+
+    var r = 50;
+    var g = 0;
+    var b = 0;
 
     var a = (y * id.width + x) * 4;
     pixels[a] = r;
     pixels[a + 1] = g;
     pixels[a + 2] = b;
     pixels[a + 3] = 255;
-
-  ctx.putImageData(id, 0, 0);
+    
+    ctx.putImageData(id, 0, 0);
 }
