3681feb9a8627f6c62083bb0384315ad22f69c97
Merkury32 <pjozefiak32@gmail.com>
2020-02-27 14:37:42 +0100

    usprawniłem js żeby móc zmieniać kolor piksela

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
-function putPixel() {
-    var canvas = document.getElementById("canvas");
-    var ctx = canvas.getContext("2d");
-    ctx.beginPath();
-    ctx.fillRect(25,25,1,1);
-    ctx.stroke();
-  }
+function putPixel(x, y, r, g, b, a) {
+  var canvas = document.getElementById('canvas');
+  var ctx = canvas.getContext('2d');
+  var canvasWidth = canvas.width;
+  var canvasHeight = canvas.height;
+  ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+  var id = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
+  var pixels = id.data;
+
+    var a = (y * id.width + x) * 4;
+    pixels[a] = r;
+    pixels[a + 1] = g;
+    pixels[a + 2] = b;
+    pixels[a + 3] = 255;
+
+  ctx.putImageData(id, 0, 0);
+}
