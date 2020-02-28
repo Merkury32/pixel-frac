c7e1d69f6d340f607ab6b62e2bee44bedefc6cd4
Merkury32 <pjozefiak32@gmail.com>
2020-02-28 15:50:45 +0100

    Dodałem działający color picker

--- main.js
+++ main.js
@@ -14,14 +14,26 @@ function putPixel() {
     var x = parseInt(tmpa);
     var y = parseInt(tmpb);
 
-    var r = 50;
-    var g = 0;
-    var b = 0;
+    var hex = document.getElementById("color-picker").value;
+    var tmp1 = parseInt(hex, 10);
+
+    function hex2r(hexa){
+      var r = parseInt(hexa.slice(1,3), 16);
+      return r;
+    }
+    function hex2g(hexa){
+      var g = parseInt(hexa.slice(3,5), 16);
+      return g;
+    }
+    function hex2b(hexa){
+      var b = parseInt(hexa.slice(5,7), 16);
+      return b;
+    }
 
     var a = (y * id.width + x) * 4;
-    pixels[a] = r;
-    pixels[a + 1] = g;
-    pixels[a + 2] = b;
+    pixels[a] = hex2r(hex);
+    pixels[a + 1] = hex2g(hex);
+    pixels[a + 2] = hex2b(hex);
     pixels[a + 3] = 255;
     
     ctx.putImageData(id, 0, 0);
