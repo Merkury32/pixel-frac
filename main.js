164b0177be0143fec061b6e4aebc076e7f404d7a
Merkury32 <pjozefiak32@gmail.com>
2020-03-05 16:39:09 +0100

    dodałem funckję drawLine która rysuje linie

--- main.js
+++ main.js
@@ -3,44 +3,54 @@ function pobierz(tmpa, tmpb){
   var tmpb = yInput.value;
   return tmpa, tmpb;
 }
-function putPixel(x,y,r,g,b,a) {
-  debugger;
+function drawLine(){
+  var context = canvas.getContext("2d");
+	context.beginPath();
+	context.moveTo(0, 0);
+	context.lineTo(100, 20);
+	context.lineWidth = 1;
+	context.strokeStyle = '#000';
+  context.stroke();
   
-  var tmpa = pobierz(tmpa);
-  var tmpb = pobierz(tmpb);
-  var hex = colorPicker.value;
-
-  var canvas = document.getElementById('canvas');
-  var ctx = canvas.getContext('2d');
-
-  var canvasWidth = canvas.width;
-  var canvasHeight = canvas.height;
-  var id = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
-  var pixels = id.data;
-
-    var x = parseInt(tmpa);
-    var y = parseInt(tmpb);
-
-    var tmpc = parseInt(hex, 10);
-
-    function hex2r(hexa){
-      var r = parseInt(hexa.slice(1,3), 16);
-      return r;
-    }
-    function hex2g(hexa){
-      var g = parseInt(hexa.slice(3,5), 16);
-      return g;
-    }
-    function hex2b(hexa){
-      var b = parseInt(hexa.slice(5,7), 16);
-      return b;
-    }
-
-    var a = (y * id.width + x) * 4;
-    pixels[a] = hex2r(hex);
-    pixels[a + 1] = hex2g(hex);
-    pixels[a + 2] = hex2b(hex);
-    pixels[a + 3] = 255;
+  function putPixel(x,y,r,g,b,a) {
+    debugger;
     
-    ctx.putImageData(id, 0, 0);
+    var tmpa = pobierz(tmpa);
+    var tmpb = pobierz(tmpb);
+    var hex = colorPicker.value;
+  
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+  
+    var canvasWidth = canvas.width;
+    var canvasHeight = canvas.height;
+    var id = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
+    var pixels = id.data;
+  
+      var x = parseInt(tmpa);
+      var y = parseInt(tmpb);
+  
+      var tmpc = parseInt(hex, 10);
+  
+      function hex2r(hexa){
+        var r = parseInt(hexa.slice(1,3), 16);
+        return r;
+      }
+      function hex2g(hexa){
+        var g = parseInt(hexa.slice(3,5), 16);
+        return g;
+      }
+      function hex2b(hexa){
+        var b = parseInt(hexa.slice(5,7), 16);
+        return b;
+      }
+  
+      var a = (y * id.width + x) * 4;
+      pixels[a] = hex2r(hex);
+      pixels[a + 1] = hex2g(hex);
+      pixels[a + 2] = hex2b(hex);
+      pixels[a + 3] = 255;
+      
+      ctx.putImageData(id, 0, 0);
+  }
 }
