4b228149331dc3c3f880a80a5db3239edeb9dc11
Merkury32 <pjozefiak32@gmail.com>
2020-03-12 15:08:10 +0100

    dodałem funkcję hexToRgb() któa zamienia kolor hex na rgb

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 function pobierz(tmpa, tmpb){
+
   var tmpa = xInput.value;
   var tmpb = yInput.value;
   return tmpa, tmpb;
-}
+};
 
 function putPixel(x,y,r,g,b,a) {
 
@@ -14,29 +15,26 @@ function putPixel(x,y,r,g,b,a) {
   var id = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
   var pixels = id.data;
 
-    var x;
-    var y;
-
-    var z = (x * id.width + y) * 4;
-    pixels[z] = r*255;
-    pixels[z + 1] = g*255;
-    pixels[z + 2] = b*255;
-    pixels[z + 3] = a;
+  var z = (x * id.width + y) * 4;
+  pixels[z] = Math.round(r*255);
+  pixels[z + 1] = Math.round(g*255);
+  pixels[z + 2] = Math.round(b*255);
+  pixels[z + 3] = Math.round(a*255);
     
-    ctx.putImageData(id, 0, 0);
-}
+  ctx.putImageData(id, 0, 0);
+};
 
 function drawLine(x1,y1,x2,y2,r,g,b){
-  debugger;
-  var x1 = parseInt(x1Input.value, 10)
-  var y1 = parseInt(y1Input.value, 10)
-  var x2 = parseInt(x2Input.value, 10)
-  var y2 = parseInt(y2Input.value, 10)
 
   var r = 0;
   var g = 0;
   var b = 1;
-  var a = 255;
+  var a = 1;
+
+  var x1 = parseInt(x1Input.value, 10);
+  var y1 = parseInt(y1Input.value, 10);
+  var x2 = parseInt(x2Input.value, 10);
+  var y2 = parseInt(y2Input.value, 10);
 
   var vx = x2-x1;
   var vy = y2-y1;
@@ -49,5 +47,25 @@ function drawLine(x1,y1,x2,y2,r,g,b){
     var xn = x1 + s*vx;
     var yn = y1 + s*vy;
     putPixel(Math.round(xn), Math.round(yn),r,g,b,a);
-  }
-}
+  };
+};
+
+function hexToRgb(){
+  
+  var hex = colorPicker.value;
+  var hex = hex.substring(1);
+  var a = alphaPicker.value;
+
+  var r = hex.slice(0,2);
+  var g = hex.slice(2,4);
+  var b = hex.slice(4,6);
+
+  var r = parseInt(r,16) /255;
+  var g = parseInt(g,16) /255;
+  var b = parseInt(b,16) /255;
+  var a = parseInt(a,10) /255;
+
+  var rgba = [r,g,b,a]
+
+  console.log(hex,rgba[1],rgba[2],rgba[3],rgba[4]);
+};
